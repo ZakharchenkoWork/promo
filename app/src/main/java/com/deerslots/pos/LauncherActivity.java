7698e78bf8698e758e853b736b08ea0ef22610a1
Natali Zakharchenko <natali.pi.np@gmail.com>
2018-12-25 00:10:38 +0200

    Changed loading gif

--- app/src/main/java/com/deerslots/pos/LauncherActivity.java
+++ app/src/main/java/com/deerslots/pos/LauncherActivity.java
@@ -28,7 +28,7 @@ public class LauncherActivity extends Activity {
         startActivity(new Intent(LauncherActivity.this, CoreActivity.class)
                 .putExtra(INTENT_ADDRESS, "http://addve.deerslots.club/index.php")
                 .putExtra(INTENT_DRAWABLE, "file:///android_asset/loading.gif")
-                .putExtra(INTENT_COLOR, "#EAE5E4")
+                .putExtra(INTENT_COLOR, "#FFFFFF")
                 .putExtra(INTENT_CLASS, MainActivity.class)
                 .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
 
